944c0b55607fa41983dd69c2070bdd7d2b9e0380
gulshan kumar <gk4644771@gmail.com>
2026-07-02 01:12:48 +0530

    config file changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,12 +8,12 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://codesoft-nextask.onrender.com',
         changeOrigin: true,
         secure: false,
       },
       '/uploads': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://codesoft-nextask.onrender.com',
         changeOrigin: true,
         secure: false,
       },
